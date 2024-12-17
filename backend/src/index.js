e8fdfe9a772e874f2ba328613e0a9bfa1366eaf5
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-17 23:33:23 +0530

    feat: using user model create signup functionality with JWT token with addition security and conditional res + test with postman

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,11 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 50001;
+const PORT = process.env.PORT || 5001;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);
   connectDB();
