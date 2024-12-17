cfd1d39b60698a1c6345bd37dd51acec9f32718a
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-17 10:53:33 +0530

    feat: connect db + create base auth setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 50001;
 
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);
+  connectDB();
 });
