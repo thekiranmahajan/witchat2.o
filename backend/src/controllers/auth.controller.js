e115f386d79f332bb2bf4d1e628fbf70cd848232
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-18 10:32:00 +0530

    feat: create login controller with conditions and secure instructions for malicious users and test it using postman

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -43,15 +43,43 @@ export const signup = async (req, res) => {
       res.status(400).json({ message: "Invalid user data" });
     }
   } catch (error) {
-    console.log("Error in signup controller", error.message);
+    console.log("Error in signup controller: ", error.message);
     res.status(500).json({
       message: "Internal Server Error",
     });
   }
 };
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordCorrect = await bycrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+    
+  } catch (error) {
+    console.log("Error in login controller: ", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
-export const logout = (req, res) => {
-  res.send("logout route");
-};
+
+export const logout = async (req, res) => {};
