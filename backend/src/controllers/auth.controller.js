d64186f245af84e798da86f23006f192b8f369af
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-18 17:38:28 +0530

    feat: create checkAuth controller to check the loggedin user status + fix the protectRoute middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,6 +113,15 @@ export const updateProfile = async (req, res) => {
     res.status(200).json(updatedUser);
   } catch (error) {
     console.log("Error in updateProfile controller: ", error.message);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
