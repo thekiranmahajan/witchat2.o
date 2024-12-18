adeb25c558e86358beefc724f9030ca74da564f7
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-18 17:17:45 +0530

    feat: create controller to securely update the profile picture of logged in user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bycrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -93,6 +94,25 @@ export const logout = async (req, res) => {
   }
 };
 
-export const updateProfile = (req, res) => {
-  res.send("updateProfile");
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in updateProfile controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
