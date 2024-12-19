dae2fda3ab0af63ff5248e79c303b786f1fee1dc
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-19 11:57:26 +0530

    feat: add sendMessage controller to handle sending messages with optional image upload

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Messsage from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -35,6 +36,36 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Messsage({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    // todo: socket.io implementation goes here for realtime messages
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,10 +3,12 @@ import protectRoute from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUsersForSidebar,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+router.get("/send/:id", protectRoute, sendMessage);
 export default router;
