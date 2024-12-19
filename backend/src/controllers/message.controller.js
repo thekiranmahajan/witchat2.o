cd1341fc0a403e4fd21e2a6e0008240103a28254
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-19 11:42:54 +0530

    feat: add message routes and controller to fetch users for sidebar

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import Messsage from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);
