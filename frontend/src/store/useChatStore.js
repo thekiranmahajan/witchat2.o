6ab6c79e929602a184aa40f7911ee29ddb3504e6
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-21 18:23:53 +0530

    feat: implement chat store with Zustand for managing messages and users

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../utils/axiosInstance";
+
+const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const { data } = await axiosInstance.get("/messages/users");
+      set({ users: data });
+    } catch (error) {
+      console.log("Error in getUsers", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const { data } = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: data });
+    } catch (error) {
+      console.log("Error in getMessages", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
+export default useChatStore;
