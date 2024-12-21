8078bfcadedfbd48a2e47ad0e05382472add3d4f
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-21 16:14:23 +0530

    fix: request body size limit for JSON and URL-encoded data to upload larger profile pictures

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,9 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
