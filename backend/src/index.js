1bd676687ffa50f9b521bbcee54a47e58e11091b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-22 16:27:57 +0530

    fix: path issue in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,10 @@ app.use("/api/messages", messageRoutes);
 
 /*********PRODUCTION CODE**********/
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 /*********PRODUCTION CODE**********/
