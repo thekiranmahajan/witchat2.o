9bb387803779df5ac80f4857498c9156069cf64f
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-22 20:38:19 +0530

    feat: add sendMessage function to chat store for message sending functionality

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import axiosInstance from "../lib/axiosInstance";
 
-const useChatStore = create((set) => ({
+const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -32,6 +32,18 @@ const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const { data } = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData,
+      );
+      set({ messages: [...messages, data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
   setSelectedUser: (userId) => {
