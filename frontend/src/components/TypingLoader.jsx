f7da1d022aff6bda348113f0eb84f4956d2a25c3
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-28 17:08:01 +0530

    feat: add TypingLoader component for indicating typing status

---
+++ frontend/src/components/TypingLoader.jsx
@@ -0,0 +1,9 @@
+const TypingLoader = () => {
+  return (
+    <div className="absolute -top-3.5 left-1 flex h-6 w-10 items-center justify-center rounded-xl bg-primary">
+      <span className="loading loading-dots loading-md bg-primary-content"></span>
+    </div>
+  );
+};
+
+export default TypingLoader;
