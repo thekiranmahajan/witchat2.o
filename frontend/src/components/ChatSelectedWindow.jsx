b61495b0a95581134218064bfc3265cad8b83553
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-29 14:06:07 +0530

    refactor: streamline message rendering and enhance readability in ChatSelectedWindow

--- frontend/src/components/ChatSelectedWindow.jsx
+++ frontend/src/components/ChatSelectedWindow.jsx
@@ -29,7 +29,7 @@ const ChatSelectedWindow = () => {
         block: "center",
       });
       setHighlightedMessageId(messageId);
-      setTimeout(() => setHighlightedMessageId(null), 2000);
+      setTimeout(() => setHighlightedMessageId(null), 1000);
     }
   }, []);
 
@@ -61,65 +61,76 @@ const ChatSelectedWindow = () => {
         <MessageSkeleton />
       ) : (
         <div className="flex-1 space-y-2.5 overflow-y-auto overflow-x-hidden !scroll-smooth p-4 md:space-y-4">
-          {messages.map((message) => (
-            <div
-              key={message._id}
-              ref={(el) => (messageRefs.current[message._id] = el)}
-              className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"} ${highlightedMessageId === message._id ? "rounded-lg bg-base-300 transition-colors duration-500" : ""}`}
-              onDoubleClick={() => setReplyMessage(message)}
-            >
-              <div className="avatar chat-image">
-                <div className="size-7 rounded-full border md:size-10">
-                  <img
-                    src={
-                      message.senderId === authUser._id
-                        ? authUser.profilePic
-                        : selectedUser.profilePic
-                    }
-                    alt="Profile Picture"
-                  />
+          {messages.map((message) => {
+            const {
+              _id,
+              createdAt,
+              senderId,
+              image = null,
+              text = "",
+              repliedMessage = null,
+            } = message;
+
+            return (
+              <div
+                key={_id}
+                className={`chat ${senderId === authUser._id ? "chat-end" : "chat-start"} ${highlightedMessageId === _id ? "rounded-lg bg-base-300 transition-colors duration-500" : ""}`}
+              >
+                <div className="avatar chat-image">
+                  <div className="size-7 rounded-full border md:size-10">
+                    <img
+                      src={
+                        senderId === authUser._id
+                          ? authUser.profilePic
+                          : selectedUser.profilePic
+                      }
+                      alt="Profile Picture"
+                    />
+                  </div>
+                </div>
+                <div className="chat-header mb-1">
+                  <time className="ml-1 text-xs opacity-50">
+                    {formatMessageTime(createdAt)}
+                  </time>
+                </div>
+                <div
+                  onDoubleClick={() => setReplyMessage(message)}
+                  ref={(el) => (messageRefs.current[_id] = el)}
+                  className={`chat-bubble flex flex-col break-words ${senderId === authUser._id ? "chat-bubble-secondary text-secondary-content" : "chat-bubble-primary text-primary-content"}`}
+                >
+                  {repliedMessage && (
+                    <div
+                      onClick={() => scrollToMessage(repliedMessage._id)}
+                      className="mb-2 cursor-pointer rounded border-l-4 border-info/80 bg-neutral p-2 transition-colors"
+                    >
+                      <p className="truncate text-sm text-neutral-content/50">
+                        {repliedMessage.text ||
+                          (repliedMessage.image ? "📷 Image" : "Message")}
+                      </p>
+                    </div>
+                  )}
+                  {image && (
+                    <img
+                      src={image}
+                      alt="Attachment"
+                      className="mb-2 max-w-52 rounded-md"
+                    />
+                  )}
+                  {text && (
+                    <p
+                      className="flex flex-col whitespace-pre-wrap break-words"
+                      dangerouslySetInnerHTML={{
+                        __html: text.replace(
+                          /(https?:\/\/[^\s]+)/g,
+                          '<a href="$1" target="_blank" class="text-base-100 underline break-words">$1</a>',
+                        ),
+                      }}
+                    ></p>
+                  )}
                 </div>
               </div>
-              <div className="chat-header mb-1">
-                <time className="ml-1 text-xs opacity-50">
-                  {formatMessageTime(message.createdAt)}
-                </time>
-              </div>
-              <div
-                className={`chat-bubble flex flex-col break-words ${message.senderId === authUser._id ? "chat-bubble-secondary text-secondary-content" : "chat-bubble-primary text-primary-content"}`}
-              >
-                {message.repliedMessage && (
-                  <div
-                    onClick={() => scrollToMessage(message.repliedMessage._id)}
-                    className="mb-2 cursor-pointer rounded border-l-4 border-info/80 bg-neutral p-2 transition-colors"
-                  >
-                    <p className="text-sm text-neutral-content/50">
-                      {message.repliedMessage.text ||
-                        (message.repliedMessage.image ? "📷 Image" : "Message")}
-                    </p>
-                  </div>
-                )}
-                {message.image && (
-                  <img
-                    src={message.image}
-                    alt="Attachment"
-                    className="mb-2 max-w-52 rounded-md"
-                  />
-                )}
-                {message.text && (
-                  <p
-                    className="flex flex-col whitespace-pre-wrap break-words"
-                    dangerouslySetInnerHTML={{
-                      __html: message.text.replace(
-                        /(https?:\/\/[^\s]+)/g,
-                        '<a href="$1" target="_blank" class="text-base-100 underline break-words">$1</a>',
-                      ),
-                    }}
-                  ></p>
-                )}
-              </div>
-            </div>
-          ))}
+            );
+          })}
           <div ref={messageEndRef} />
         </div>
       )}
