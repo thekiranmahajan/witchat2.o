de881526e80f46b0152cb2928acaef62ce75afaf
Kiran Mahajan <thekiranmahajan@outlook.com>
2025-12-16 18:23:03 +0530

    feat: remove avatar from chat

--- frontend/src/components/ChatSelectedWindow.jsx
+++ frontend/src/components/ChatSelectedWindow.jsx
@@ -61,72 +61,61 @@ const ChatSelectedWindow = () => {
         <MessageSkeleton />
       ) : (
         <div className="flex-1 space-y-2.5 overflow-y-auto overflow-x-hidden !scroll-smooth p-4 md:space-y-4">
-          {Array.isArray(messages) && messages.map((message) => {
-            const {
-              _id,
-              createdAt,
-              senderId,
-              image = null,
-              text = "",
-              repliedMessage = null,
-            } = message;
+          {Array.isArray(messages) &&
+            messages.map((message) => {
+              const {
+                _id,
+                createdAt,
+                senderId,
+                image = null,
+                text = "",
+                repliedMessage = null,
+              } = message;
 
-            return (
-              <div
-                key={_id}
-                className={`chat ${senderId === authUser._id ? "chat-end" : "chat-start"} ${highlightedMessageId === _id ? "rounded-lg bg-base-300 transition-colors duration-500" : ""}`}
-              >
-                <div className="avatar chat-image">
-                  <div className="size-7 rounded-full border md:size-10">
-                    <img
-                      src={
-                        senderId === authUser._id
-                          ? authUser.profilePic
-                          : selectedUser.profilePic
-                      }
-                      alt="Profile Picture"
-                    />
+              return (
+                <div
+                  key={_id}
+                  className={`chat ${senderId === authUser._id ? "chat-end" : "chat-start"} ${highlightedMessageId === _id ? "rounded-lg bg-base-300 transition-colors duration-500" : ""}`}
+                >
+                  <div className="chat-header mb-1">
+                    <time className="ml-1 text-xs opacity-50">
+                      {formatMessageTime(createdAt)}
+                    </time>
+                  </div>
+                  <div
+                    onDoubleClick={() => setReplyMessage(message)}
+                    ref={(el) => (messageRefs.current[_id] = el)}
+                    className={`chat-bubble flex flex-col break-words ${senderId === authUser._id ? "chat-bubble-secondary text-secondary-content" : "chat-bubble-primary text-primary-content"}`}
+                  >
+                    {repliedMessage && (
+                      <div
+                        onClick={() => scrollToMessage(repliedMessage._id)}
+                        className="mb-2 cursor-pointer rounded border-l-4 border-info/80 bg-neutral p-2 transition-colors"
+                      >
+                        <p className="truncate text-sm text-neutral-content/50">
+                          {repliedMessage.text ||
+                            (repliedMessage.image ? "📷 Image" : "Message")}
+                        </p>
+                      </div>
+                    )}
+                    {image && (
+                      <img
+                        src={image}
+                        alt="Attachment"
+                        className="sm:max-w-1/2 mb-2 max-w-96 rounded-md"
+                      />
+                    )}
+                    {text && (
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: processMessageText(text),
+                        }}
+                      />
+                    )}
                   </div>
                 </div>
-                <div className="chat-header mb-1">
-                  <time className="ml-1 text-xs opacity-50">
-                    {formatMessageTime(createdAt)}
-                  </time>
-                </div>
-                <div
-                  onDoubleClick={() => setReplyMessage(message)}
-                  ref={(el) => (messageRefs.current[_id] = el)}
-                  className={`chat-bubble flex flex-col break-words ${senderId === authUser._id ? "chat-bubble-secondary text-secondary-content" : "chat-bubble-primary text-primary-content"}`}
-                >
-                  {repliedMessage && (
-                    <div
-                      onClick={() => scrollToMessage(repliedMessage._id)}
-                      className="mb-2 cursor-pointer rounded border-l-4 border-info/80 bg-neutral p-2 transition-colors"
-                    >
-                      <p className="truncate text-sm text-neutral-content/50">
-                        {repliedMessage.text ||
-                          (repliedMessage.image ? "📷 Image" : "Message")}
-                      </p>
-                    </div>
-                  )}
-                  {image && (
-                    <img
-                      src={image}
-                      alt="Attachment"
-                      className="mb-2 max-w-52 rounded-md"
-                    />
-                  )}
-                  {text && (
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: processMessageText(text),
-                      }}
-                    />
-                  )}
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
           <div ref={messageEndRef} />
         </div>
       )}
